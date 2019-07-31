92878dd7fe9f96c3c1f9e569dccdd4a83f68e272
Eugene <eugeneaad@gmail.com>
2019-07-31 23:06:27 +0800

    completed inner booking module

--- models/bookings.js
+++ models/bookings.js
@@ -7,11 +7,12 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var BookingSchema = new Schema({
-    serviceName: { type: String, unique: true, required: true },
-    start: { type: Number },
-    end: { type: Number },
+    serviceName: { type: String, required: true },
+    start: { type: Date, required: true, index: true },
+    end: { type: Date, required: true, index: true },
     staff: { type: Schema.Types.ObjectId, ref: 'Staff', required: true },
+    allDay: {type: Boolean, index: true, default: false},
     delFlag: { type: Boolean, index: true, default: false },
 }, { timestamps: true });
 
-module.exports = mongoose.model('Service', BookingSchema);
+module.exports = mongoose.model('Booking', BookingSchema);
--- routes/apis/bookingMgt.js
+++ routes/apis/bookingMgt.js
@@ -4,25 +4,107 @@ let createError = require('http-errors');
 
 
 let Staff = require('../../models/auth/staff');
-let Client = require('../../models/auth/client');
+let Booking = require('../../models/bookings');
 let auth = require('../../services/auth');
 let logger = require('../../services/logger');
 
-router.get('/clients', async (reqe, res, next) => {
+/* GET booking list. */
+router.get('/bookings', async (reqe, res, next) => {
     let staff = await Staff.findById(res.locals.user.id).populate('role');
-    if (!staff.role.staffMgt.list) { next(createError(403)); return; }
+    if (!staff.role.bookingMgt.list) { next(createError(403)); return; }
 
-    //get raw data from data
-    let rawClients = await Client.find({ "delFlag": false }).lean()
-        .select({
-            "username": 1,
-            "email": 1,
-            "mobile": 1,
-            "displayName": 1,
-            "createdAt": 1,
-        });
+    // return and rename the data as calender needs
+    let bookings = await Booking.aggregate([
+        {$match: {delFlag: false}},
+        {
+            $project: {
+                "id": "$_id",
+                "title": "$serviceName",
+                "resourceId": "$staff",
+                "start" : 1,
+                "end": 1,
+                "allDay": 1,
+            }
+        }
+    ])
 
-    res.send(rawClients);
+    res.send(bookings);
+});
+
+/* POST Create booking. */
+router.post('/bookings', async (reqe, res, next) => {
+    try {
+
+        let staff = await Staff.findById(res.locals.user.id).populate('role');
+        if (!staff.role.bookingMgt.create) { next(createError(403)); return; }
+
+        //load main fields
+        let booking = new Booking(reqe.body);
+        booking.createdBy = staff._id;
+        booking.updatedBy = staff._id;
+
+        //save client 
+        let doc = await booking.save();
+        let rsObj = { ok: "Booking has been created.", id: doc._id };
+        logger.audit("Booking Mgt", "Create", doc._id, staff.id, `A new booking has been created by ${staff.displayName}`);
+        res.json(rsObj);
+
+    } catch (err) { res.status(400).json({ error: `Cannot create booking, ${err.message}` }) }
+
+});
+
+/* PATCH update booking. */
+router.patch('/bookings/:id', async (reqe, res, next) => {
+    try {
+
+        let staff = await Staff.findById(res.locals.user.id).populate('role');
+        if (!staff.role.bookingMgt.edit) { next(createError(403)); return; }
+
+        let rawBooking = reqe.body;
+
+        //load booking from db
+        let booking = await Booking.findOne({ "_id": reqe.params.id, "delFlag": false });
+
+        booking.updatedBy = staff._id;
+        // booking.serviceName = rawBooking.serviceName || booking.serviceName;
+        booking.start = rawBooking.start || booking.start;
+        booking.end = rawBooking.end || booking.end;
+        booking.staff = rawBooking.staff || booking.staff;
+        booking.allDay = rawBooking.allDay || booking.allDay;
+
+        //save booking 
+        let doc = await booking.save();
+        let rsObj = { ok: "Booking has been updated.", id: doc._id };
+        logger.audit("Booking Mgt", "Update", doc._id, staff.id, `Booking has been updated by ${staff.displayName}`);
+        res.json(rsObj);
+
+    } catch (err) { res.status(400).json({ error: `Cannot update booking, ${err.message}` }); }
+
+});
+
+/* DELETE disable booking. */
+router.delete('/bookings/:id', async (reqe, res, next) => {
+    try {
+
+        let staff = await Staff.findById(res.locals.user.id).populate('role');
+        if (!staff.role.bookingMgt.delete) { next(createError(403)); return; }
+
+
+        //load data from db
+        let booking = await Booking.findOne({ "_id" : reqe.params.id});
+        booking.updatedBy = staff._id;
+        booking.delFlag = true;
+        // //save user 
+        let doc = await booking.save();
+        let rsObj = {
+            ok      : "User has been deleted.", 
+            id      : doc._id
+        };
+        logger.audit("Booking Mgt", "Delete", doc._id, staff.id, `Booking has been deleted by ${staff.DisplayName}`);
+        res.json(rsObj);
+
+    } catch (err) { res.status(400).json({ error: `Cannot delete booking, ${err.message}` }) }
+
 });
 
 module.exports = router;
